1af3cb88877fd32eb2b00fa1df466cddd4b06b63
weiersyuan <qhyuan1992@gmail.com>
2018-05-29 15:12:05 +0800

    delete useless files

--- src/main/java/com/memory/analysis/Main.java
+++ src/main/java/com/memory/analysis/Main.java
@@ -5,11 +5,8 @@ import com.memory.analysis.process.ClassAnalysis;
 import com.memory.analysis.process.ClassObjWrapper;
 import com.memory.analysis.process.InstanceAnalysis;
 import com.memory.analysis.process.InstanceWrapper;
-import com.memory.analysis.utils.Constants;
 import com.memory.analysis.utils.StableList;
-import com.squareup.haha.perflib.ClassObj;
 import com.squareup.haha.perflib.HprofParser;
-import com.squareup.haha.perflib.Instance;
 import com.squareup.haha.perflib.Snapshot;
 import com.squareup.haha.perflib.io.HprofBuffer;
 import com.squareup.haha.perflib.io.MemoryMappedFileBuffer;
@@ -57,14 +54,6 @@ public class Main {
 
         instanceThread.start();
         classThread.start();
-    }
-
-    private static void findMayActivityLeak(Snapshot snapshot) {
-        ClassObj activityClassObj = snapshot.findClass(Constants.ANDROID_BACTIVITY_CLASS);
-
-        for (Instance instance : activityClassObj.getInstancesList()) {
-            System.out.println(instance.getClassObj().getClassName());
-        }
     }
 
     static class InstanceRunnable implements Runnable{
@@ -120,57 +109,4 @@ public class Main {
         }
     }
 
-
-
-
-
-    public void test() {
-
-//        snapshot.dumpInstanceCounts();
-//        snapshot.dumpSizes();
-//        snapshot.getGCRoots();
-
-//        snapshot.computeDominators();
-//            List<Instance> instanceList = snapshot.getReachableInstances();
-//        for (Instance instance : instanceList) {
-////            System.out.println(Long.toHexString(instance.getId()));
-//        }
-//        Instance i = snapshot.findInstance(0x1315bf10);
-//        System.out.println(i.getTotalRetainedSize());
-//        System.out.println(i.getSize());
-
-
-        /**
-         * 0x13539d60
-         * android.graphics.Bitmapleak 0.8898191452026367M
-         * GC ROOT static com.facebook.common.references.SharedReference.sLiveObjects
-         * references java.util.IdentityHashMap.table
-         * references array java.lang.Object[].[578]
-         * leaks android.graphics.Bitmap instance
-         *
-         *
-         * 0x1315bf10
-         * android.graphics.Bitmapleak 1.1574506759643555M
-         * GC ROOT thread android.os.HandlerThread.<Java Local> (named 'DFM Handler Thread #0')
-         * references master.flame.danmaku.controller.DrawHandler.mDanmakuView
-         * references master.flame.danmaku.ui.widget.DanmakuView.mResources
-         * references android.content.res.Resources.mResourcesImpl
-         * references android.content.res.ResourcesImpl.mDrawableCache
-         * references android.content.res.DrawableCache.mUnthemedEntries
-         * references android.util.LongSparseArray.mValues
-         * references array java.lang.Object[].[131]
-         * references java.lang.ref.WeakReference.referent
-         * references android.graphics.drawable.BitmapDrawable$BitmapState.mBitmap
-         * leaks android.graphics.Bitmap instance
-         *
-         */
-
-        // 0x13539d60    0x1315bf10
-        /*Instance i = snapshot.findInstance(0x1315bf10);//0x1315bf10
-        HeapAnalyzer heapAnalyzer = new HeapAnalyzer();
-        AnalysisResult result = heapAnalyzer.findLeakTrace(0, snapshot, i);
-        System.out.println(result.className + "leak " + result.retainedHeapSize /1024.0/1024.0 + "M");
-        System.out.println(result.leakTrace.toString());*/
-
-    }
 }
